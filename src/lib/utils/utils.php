86ef7725433dd5b3ea62ab87834563ea38a48cd8
matidau <65836048+matidau@users.noreply.github.com>
2025-04-14 16:16:58 +1000

    AS16.1 - fix import message fails if it was deleted on the server
    AS16.1 changes based on grommunio-sync commit
    https://github.com/grommunio/grommunio-sync/commit/1d7d632620979e7a1459790c4dbd8dd37524b50d

--- src/lib/utils/utils.php
+++ src/lib/utils/utils.php
@@ -1523,6 +1523,36 @@ class Utils {
         }
         return 0;
     }
+
+    /**
+	 * Returns the appropriate SyncObjectResponse object based on message class.
+	 *
+	 * @param string $messageClass
+	 *
+	 * @return object
+	 */
+	public static function GetResponseFromMessageClass($messageClass) {
+		$messageClass = strtolower($messageClass);
+
+		switch ($messageClass) {
+			case 'syncappointment':
+				return new SyncAppointmentResponse();
+
+			case 'synccontact':
+				return new SyncContactResponse();
+
+			case 'syncnote':
+				return new SyncNoteResponse();
+
+			case 'synctask':
+				return new SyncTaskResponse();
+
+			default:
+				return new SyncMailResponse();
+		}
+
+		return new SyncMailResponse();
+	}
 }
 
 // TODO Win1252/UTF8 functions are deprecated and will be removed sometime
