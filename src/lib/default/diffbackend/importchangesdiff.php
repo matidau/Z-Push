40ce23e527e7db6d44f4dd6b7811f9fb641aa589
matidau <65836048+matidau@users.noreply.github.com>
2025-04-19 09:44:50 +1000

    AS16.1 - set importchangesdiff set serverid

--- src/lib/default/diffbackend/importchangesdiff.php
+++ src/lib/default/diffbackend/importchangesdiff.php
@@ -133,6 +133,9 @@ class ImportChangesDiff extends DiffState implements IImportChanges {
             ZLog::Write(LOGLEVEL_WARN, sprintf("ImportChangesDiff->ImportMessageChange() GetProtocolVersion() >= 16.0"));
             $response = $this->backend->GetMessage($this->folderid, $stat["id"], $this->contentparameters);
             $response = Utils::GetResponseFromObject($response);
+            if (property_exists($response, "serverid")) {
+                $response->serverid = $stat["id"];
+            }            
             if (property_exists($response, "hasResponse")) {
                 $response->hasResponse = true;
             }
