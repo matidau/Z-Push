99c1565bd38382f37b641700c4abd5d5e8b7d154
matidau <65836048+matidau@users.noreply.github.com>
2025-04-19 13:19:50 +1000

    Revert "AS16.1 - IMAP ChangeMessage isdraft for updates"
    This reverts commit 64eba582705a15d00ee635b650c1f6a95c600219.

--- src/backend/imap/imap.php
+++ src/backend/imap/imap.php
@@ -1675,7 +1675,7 @@ class BackendIMAP extends BackendDiff implements ISearchProvider {
         // TODO this could throw several StatusExceptions like e.g. SYNC_STATUS_OBJECTNOTFOUND, SYNC_STATUS_SYNCCANNOTBECOMPLETED
 
         // 'draft'
-        if(!$id || (isset($message->isdraft) && $message->isdraft)) {
+        if(!$id) {
             ZLog::Write(LOGLEVEL_DEBUG, sprintf("BackendIMAP->ChangeMessage(): Save Draft"));
             
             $saved = $this->saveDraftMail($message);
