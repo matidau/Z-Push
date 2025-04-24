8dc7d42d9ae9df67c40250264755baa789f57a24
matidau <65836048+matidau@users.noreply.github.com>
2025-04-24 15:40:35 +1000

    AS16.1 - IMAP getDraftMessageId array keys

--- src/backend/imap/imap.php
+++ src/backend/imap/imap.php
@@ -3392,8 +3392,8 @@ class BackendIMAP extends BackendDiff implements ISearchProvider {
                 ZLog::Write(LOGLEVEL_WARN, sprintf("BackendIMAP->getDraftMessageId() message->id: %s", $message->id));
                 ZLog::Write(LOGLEVEL_WARN, sprintf("BackendIMAP->getDraftMessageId() draftMessageId->id: %s", $message->draftMessageId));
 
-                if (isset($message->draftMessageId) && $message->draftMessageId == $id) {
-                    $returnid = $message->id;
+                if (isset($message['draftMessageId']) && $message['draftMessageId'] == $id) {
+                    $returnid = $message['id'];
                     break;
                 }
             }
